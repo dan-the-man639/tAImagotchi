2b05985350860b4b03cb17be74d97726f9a7b9b5
Ri-Hong <riri.hong@gmail.com>
2024-01-27 17:36:49 -0500

    Status bars work?

--- frontend/taimagotchi/src/components/StatBar.jsx
+++ frontend/taimagotchi/src/components/StatBar.jsx
@@ -4,15 +4,18 @@ import { useState, useEffect } from 'react';
 //dif: newHP - current HP
 
 function Stat({ statItem, value }) {
-    const numParitions = 25;
+    const numParitions = 10;
     const imageBasePath = "/images/"; // Path relative to the public directory
 
     let imageName = statItem;
     let partitionValue = Math.floor(value / numParitions) * numParitions;
 
-    imageName = imageBasePath + imageName + "_" + partitionValue + ".png";
+    imageName = imageBasePath + "bar" + "_" + partitionValue + ".png";
 
     console.log(imageName);
+
+    let iconName = imageBasePath + statItem + "_icon.png";
+    console.log(iconName)
 
     return (
         <div>
@@ -26,7 +29,23 @@ function Stat({ statItem, value }) {
                     }}
                 />
             </div>
-            <img src={imageName} alt={`${statItem} stat bar`} />
+            
+            <div style={{display: `flex`}}>
+                <img src={iconName} alt={`${statItem} icon`} 
+                    style={{
+                        height: '60px',
+                        width: '60px',
+                        objectFit: `contain`
+                    }}/>
+                <img src={imageName} alt={`${statItem} stat bar`} 
+                    style={{
+                        height: 'auto',
+                        width: '300px',
+                        objectFit: `contain`
+                    }}/>
+            </div>
+
+           
         </div>
     );
 }
