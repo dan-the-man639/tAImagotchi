059786d0c07afae8c97da22361af4d6c33d69724
dan-the-man639 <108316903+dan-the-man639@users.noreply.github.com>
2024-01-27 05:46:12 -0500

    front end

---
+++ frontend/taimagotchi/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Game from './pages/Game'
+
+function App() {
+  return (
+    <div>
+      <Game/>
+    </div>
+  );
+}
+
+export default App;
