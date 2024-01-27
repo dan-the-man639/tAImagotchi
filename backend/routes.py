0669c0b97f7e4b29903e175e8c87034d81b5223e
Ri-Hong <riri.hong@gmail.com>
2024-01-27 17:47:35 -0500

    Editing state api

--- backend/routes.py
+++ backend/routes.py
@@ -6,7 +6,8 @@ router = APIRouter()
 
 @router.get("/reset-stats")
 async def reset_stats():
-    return "Reset stats"
+    game.reset()
+    return "Game has been reset"
 
 @router.get("/get-stats")
 async def get_stats():
@@ -14,6 +15,11 @@ async def get_stats():
     vitals = stats["vitals"]
     vital_stats = [{"Type": vital["type_name"], "Stat": vital["value"]} for vital in vitals]
     return vital_stats
+
+@router.get("/get-state")
+async def get_state():
+    stats = game.get_pet_state()
+    return stats
 
 @router.get("/generate-trigger")
 async def generate_trigger():
