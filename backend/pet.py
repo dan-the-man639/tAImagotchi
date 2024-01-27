2854977820a45192b8b8794f8b62ef47bc3a12f1
Ricky Huang <rickyhuang0219@gmail.com>
2024-01-26 21:59:13 -0500

    added frontend backend folders

---
+++ backend/pet.py
@@ -0,0 +1,21 @@
+class Vitals:
+    def __init__(self, weight, height):
+        self.satiation
+        self.sanity
+        
+
+    def show(self):
+        
+
+class Pet:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age # days
+        self.vitals = Vitals(100, 100)
+
+    def show(self):
+        print(f"I am {self.name} and I am {self.age} days old.")
+
+    def speak(self):
+        print("I don't know what I say.")
+
