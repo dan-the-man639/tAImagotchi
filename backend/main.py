53a87d0603c73f2e2a34781820e8292dce508ebb
Ri-Hong <riri.hong@gmail.com>
2024-01-27 11:13:24 -0500

    Game loop works

--- backend/main.py
+++ backend/main.py
@@ -5,23 +5,31 @@ from pet import Pet
 from game import Game
 import asyncio
 
+from contextlib import asynccontextmanager
 
 app = FastAPI()
 pet = Pet.create_from_state("pet_state.json")
 game = Game(pet)
+counter = 0
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    global counter
+    # Startup logic: Starting the game loop
+    task = asyncio.create_task(start_game())
+    yield
+    # Shutdown logic: Stopping the game loop
+    task.cancel()
+
+async def start_game():
+    global counter
+    while game.is_running:
+        game.update()
+        counter += 1
+        await asyncio.sleep(1)
+
+app = FastAPI(lifespan=lifespan)
 
 @app.get("/")
 async def read_root():
-    return {"Hello": "World"}
-
-
-
-async def start_game():
-    while game.is_running:
-        # game logic
-        game.update()
-        await asyncio.sleep(1)
-        
-        
-        
-        
+    return {"Hello": str(counter)}
