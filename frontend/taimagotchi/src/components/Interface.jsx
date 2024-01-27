16afb731b63c6fbf3667732d9cba523cc2e9af2c
dan-the-man639 <108316903+dan-the-man639@users.noreply.github.com>
2024-01-27 14:16:36 -0500

    update

--- frontend/taimagotchi/src/components/Interface.jsx
+++ frontend/taimagotchi/src/components/Interface.jsx
@@ -5,26 +5,14 @@ import Typed from "react-typed";
 function Interface() {
 
     return (
-        <>
+        <div className='interface-display'>
             <div className='pet-display'>
                 <h1>Here is your pet</h1>
                 <video src={videoXD} autoPlay loop muted />
             </div>
 
-            <div className='dialog-box'>
-                <div>This is a paragraph</div>
-                <Typed
-                    strings={[
-                        "I'm a Full Stack Developer",
-                        "I Love Software Development",
-                        "I Love All My Subscribers",
-                    ]}
-                    typeSpeed={150}
-                    backSpeed={100}
-                    loop
-                />
-            </div>
-        </>
+            
+        </div>
     );
 }
 
