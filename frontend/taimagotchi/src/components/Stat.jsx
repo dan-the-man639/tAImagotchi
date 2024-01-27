d5b664566610c8025803a02b8cbf659f74ce1a1d
Ri-Hong <riri.hong@gmail.com>
2024-01-27 16:54:41 -0500

    Working with stat-bars

--- frontend/taimagotchi/src/components/Stat.jsx
+++ frontend/taimagotchi/src/components/Stat.jsx
@@ -9,10 +9,6 @@ function Stat() {
     { Type: "Energy", Stat: 100 },
     { Type: "Sanity", Stat: 100 }
   ]);
-  const [hpSatiation, setHpSatiation] = useState(100);
-  const [hpHydration, setHpHydration] = useState(100);
-  const [hpEnergy, setHpEnergy] = useState(100);
-  const [hpSanity, setHpSanity] = useState(100);
 
   // useEffect(() => {
   //   fetch("http://127.0.0.1:8000/get-stats")
@@ -27,10 +23,10 @@ function Stat() {
 
   return (
     <div className='stat-bar'>
-      <StatBar statItem={statData[0].Type} hp={hpSatiation}  onSetHp={setHpSatiation} dif={statData[0].Stat - hpSatiation }/>
-      <StatBar statItem={statData[1].Type} hp={hpHydration}  onSetHp={setHpHydration} dif={statData[1].Stat - hpHydration}/>
+      <StatBar statItem={statData[0].Type} value={statData[0].Stat}/>
+      {/* <StatBar statItem={statData[1].Type} hp={hpHydration}  onSetHp={setHpHydration} dif={statData[1].Stat - hpHydration}/>
       <StatBar statItem={statData[2].Type} hp={hpEnergy}  onSetHp={setHpEnergy} dif={statData[2].Stat - hpEnergy}/>
-      <StatBar statItem={statData[3].Type} hp={hpSanity}  onSetHp={setHpSanity} dif={statData[3].Stat - hpSanity}/>
+      <StatBar statItem={statData[3].Type} hp={hpSanity}  onSetHp={setHpSanity} dif={statData[3].Stat - hpSanity}/> */}
     </div>
   );
   }
--- frontend/taimagotchi/src/components/StatBar.jsx
+++ frontend/taimagotchi/src/components/StatBar.jsx
@@ -1,52 +1,35 @@
-
 import { useState, useEffect } from 'react';
 
 //hp: current HP
 //dif: newHP - current HP
 
-function Stat({ statItem, hp, onSetHp, dif }) {
-    //const [hp, setHp] = useState(100); // Initial HP value
-    const decreaseRate = 1; // HP decrease rate per minute
-    const increaseAmount = 2; // HP increase amount when the button is clicked
-    const[currentHp, setCurrentHp] = useState(hp + dif);
-    //const mockReturned = [
-    //     {hunger: 95},
-    //     {health: 90},
-    //     {thrist: 85},
-    //     {happiness: 80}
-    //   ]
+function Stat({ statItem, value }) {
+    const numParitions = 25;
+    const imageBasePath = "/images/"; // Path relative to the public directory
 
+    let imageName = statItem;
+    let partitionValue = Math.floor(value / numParitions) * numParitions;
 
+    imageName = imageBasePath + imageName + "_" + partitionValue + ".png";
 
-    useEffect(() => {
-            const interval = setInterval(() => {
-                // Decrease HP every minute
-                onSetHp((prevHp) => (prevHp > currentHp ? prevHp - decreaseRate : currentHp));
-            }, 1000); // 60000 milliseconds = 1 minute
-
-            return () => clearInterval(interval);
-    }, []);
-
-    useEffect(() => {
-        setCurrentHp(hp + dif);
-        onSetHp(hp + dif);
-    }, [hp, dif])
+    console.log(imageName);
 
     return (
         <div>
-            <h2>{statItem}: {currentHp}</h2>
+            <h2>{statItem}: {value}</h2>
             <div style={{ border: '1px solid #ccc', width: '300px', borderRadius: '5px', overflow: 'hidden' }}>
                 <div
                     style={{
-                        width: `${currentHp}%`,
                         height: '20px',
                         backgroundColor: 'green',
                         transition: 'width 0.5s ease',
                     }}
                 />
             </div>
+            <img src={imageName} alt={`${statItem} stat bar`} />
         </div>
     );
 }
+
 
 export default Stat;
