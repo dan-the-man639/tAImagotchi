8c07504302dc1444eea2d6f6cb519a637be4e025
Ri-Hong <riri.hong@gmail.com>
2024-01-27 23:14:13 -0500

    pulling

--- frontend/taimagotchi/src/components/TriggerBox.jsx
+++ frontend/taimagotchi/src/components/TriggerBox.jsx
@@ -3,12 +3,12 @@ import { ReactTyped } from "react-typed";
 import { useState, useEffect } from "react";
 
 function TriggerBox({ text, setText }) {
-
+    const text2 = "hello";
   return (
     <div class="parent">
       <div class="background">
         <div class="text">
-            <ReactTyped strings={[text]} typeSpeed={40} />
+            <ReactTyped strings={[text2]} typeSpeed={40} />
         </div>
       </div>
     </div>
